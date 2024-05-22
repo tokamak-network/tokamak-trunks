0e24b66e745c141a870c237a4aa3f6c805962c7e
ohbyeongmin <fbottlem@gmail.com>
2024-05-22 17:02:10 +0900

    feat: trunks service

---
+++ trunks/service.go
@@ -0,0 +1,50 @@
+package trunks
+
+import (
+	"log"
+
+	"github.com/tokamak-network/tokamak-trunks/nmgr"
+	"github.com/urfave/cli/v2"
+)
+
+type TrunksErvice struct {
+	NodeMgr nmgr.NodeManager
+}
+
+func Main() cli.ActionFunc {
+	return func(cliCtx *cli.Context) error {
+		cfg := NewCLIConfig(cliCtx)
+		service, err := NewService(cfg)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer service.Stop()
+		if err := service.Start(); err != nil {
+			return err
+		}
+		return nil
+	}
+}
+
+func NewService(cfg *CLIConfig) (*TrunksErvice, error) {
+	var svc TrunksErvice
+	if cfg.NodeManagerEnable {
+		var err error
+		svc.NodeMgr, err = nmgr.NewBaseNodeManager(
+			nmgr.NewConfig(cfg.NodeMgr),
+		)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &svc, nil
+}
+
+func (ts *TrunksErvice) Start() error {
+	ts.NodeMgr.Start()
+	return nil
+}
+
+func (ts *TrunksErvice) Stop() {
+	defer ts.NodeMgr.Destroy()
+}
