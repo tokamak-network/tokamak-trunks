51313451e6f0a3b4ec929d50bb7652a370e03ab9
ohbyeongmin <fbottlem@gmail.com>
2024-05-27 17:07:42 +0900

    feat: config add

--- trunks/config.go
+++ trunks/config.go
@@ -8,6 +8,12 @@ import (
 
 type CLIConfig struct {
 	NodeManagerEnable bool
+	L1RPC             string
+	L2RPC             string
+	L1ChainId         uint64
+	L2ChainId         uint64
+	L1StandardBrige   string
+	L2StandardBrige   string
 
 	NodeMgr nmgr.CLIConfig
 }
@@ -15,6 +21,12 @@ type CLIConfig struct {
 func NewCLIConfig(ctx *cli.Context) *CLIConfig {
 	return &CLIConfig{
 		NodeManagerEnable: ctx.Bool(flags.NodeManagerEnableFlag.Name),
+		L1RPC:             ctx.String(flags.L1RPCFlag.Name),
+		L2RPC:             ctx.String(flags.L2RPCFlag.Name),
+		L1ChainId:         ctx.Uint64(flags.L1ChainIdFlag.Name),
+		L2ChainId:         ctx.Uint64(flags.L2ChainIdFlag.Name),
+		L1StandardBrige:   ctx.String(flags.L1StandardBrige.Name),
+		L2StandardBrige:   ctx.String(flags.L2StandardBrige.Name),
 		NodeMgr:           nmgr.ReadCLIConfig(ctx),
 	}
 }
